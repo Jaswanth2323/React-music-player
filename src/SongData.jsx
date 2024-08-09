c55a216f1a5b3490820061772d9cdad639e00663
Jaswanth <105656671+Jaswanth2323@users.noreply.github.com>
2024-08-09 11:06:25 +0530

    Add files via upload

---
+++ src/SongData.jsx
@@ -0,0 +1,86 @@
+import { v4 as uuidv4 } from "uuid";
+
+function data() {
+  return [
+    {
+      audio:
+        "https://mp3teluguwap.net/mp3/2024/Devara/Devara%20-%20HQ/Fear.mp3",
+      name: "Fear song",
+      artist:"Anirudh Ravichander",
+      id: uuidv4(),
+      color: ["#EF8EA9", "#ab417f"],
+      active: true,
+      cover:
+        "https://www.naasongs.to/wp-content/uploads/2024/05/Devara-2024-Fear-Song-250x250.jpg",
+    },
+    {
+      audio:
+        "https://mp3teluguwap.net/mp3/2024/Devara/Devara/Chuttamalle.mp3",
+      name: "Chuttamalle   ",
+      id: uuidv4(),
+      artist:" Shilpa Rao",
+      color: ["#EF8EA9", "#ab417f"],
+      active: false,
+      cover:
+        "https://www.naasongs.co/wp-content/uploads/2024/08/Devara-2024-Chuttamalle-250x250.jpg",
+    },,
+    {
+      audio:
+        "https://mp3teluguwap.net/mp3/2024/Family%20Star/Family%20Star/Kalyani%20Vaccha%20Vacchaa.mp3",
+      name: "Kalyani Vaccha Vacchaa Song",
+      id: uuidv4(),
+      active: false,
+      artist:"Karthik, Mangli",
+      color: ["#EF8EA9", "#ab417f"],
+      cover:
+        "https://naasongs.com.co/wp-content/uploads/2024/03/The-Family-Star-2024.jpg",
+    },
+    {
+      audio:
+        "https://mp3teluguwap.net/mp3/2024/Hanuman/Poolamme%20Pilla.mp3",
+      name: "Poolamme Pilla",
+      id: uuidv4(),
+      active: false,
+      artist:"Gowra Hari",
+      color: ["#EF8EA9", "#ab417f"],
+      cover:
+        "https://c.saavncdn.com/807/Poolamme-Pilla-From-HanuMan-Telugu-Telugu-2024-20240620164831-500x500.jpg",
+    },
+    {
+      audio:
+        "https://mp3teluguwap.net/mp3/2023/Hi%20Nanna/Hi%20Nanna/Asalelaa.mp3",
+      name: "Asalelaa",
+      id: uuidv4(),
+      active: false,
+      artist:"Chinmayi Sripada,Anurag Kulkarni",
+      color: ["#EF8EA9", "#ab417f"],
+      cover:
+        "https://sensongsmp3.live/wp-content/uploads/2023/09/nani-hi-nanna-telugu-movie-songs.jpg",
+    },
+    {
+      audio:
+        "https://mp3teluguwap.net/mp3/2023/Hi%20Nanna/Hi%20Nanna%20-%20HQ/Ammaadi.mp3",
+      name: "Ammaadi",
+      id: uuidv4(),
+      active: false,
+      artist:"Kaala Bhairava, Shakthisree Gopalan",
+      color: ["#EF8EA9", "#ab417f"],
+      cover:
+        "https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_hungama/8903431969712_20231104114328/8903431969712/1699079417514/resources/8903431969712.jpg",
+    },
+    {
+      audio:
+        "https://mp3teluguwap.net/mp3/2024/Kalki%202989%20AD/Kalki%202989%20AD%20-%20HQ/Ta%20Takkara.mp3",
+      name: "Ta Takkara Song ",
+      id: uuidv4(),
+      active: false,
+      artist:" Sanjith Hegde, Dhee, Santhosh Narayanan",
+      color: ["#EF8EA9", "#ab417f"],
+      cover:
+        "https://s.saregama.tech/image/c/fw_485/6/ec/13/kalki_complex_telugu_ott_1440_1719566074.jpg",
+    },
+
+  ];
+}
+
+export default data;
